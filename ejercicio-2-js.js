3d0d90446990c706785781b0dfd103cd6147cfec
beacl3320 <87014830+beacl3320@users.noreply.github.com>
2022-05-23 12:51:58 +0200

    FEAT: ejercicio 2 js

---
+++ ejercicio-2-js.js
@@ -0,0 +1,16 @@
+const persona = {
+    nombre: 'Beatriz',
+    edad: 40,
+    desarrollador: true,
+    libro: {
+        titulo: 'El gran libro de HTML, CSS y JS',
+        autor: 'J.D. Gauchat',
+        fecha: 2017,
+        url: 'www.marcombo.com'
+    }
+}
+
+const fecha_valores = new Date(1982, 4, 24);
+
+console.log(persona);
+console.log(fecha_valores)
